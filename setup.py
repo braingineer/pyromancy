2d5d0ad1f6aff39f18853c9487c3c7ebabaf0d1f
brian <brian.c.mcmahan@gmail.com>
2017-05-19 09:55:38 -0400

    removing comments

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
   name = 'pyromancy',
-  packages = ['pyromancy'], # this must be the same as the name above
+  packages = ['pyromancy'], 
   version = '0.1',
   description = 'A library for PyTorch',
   author = 'Brian McMahan',
   author_email = 'brian@joostware.com',
-  url = 'https://github.com/braingineer/pyromancy', # use the URL to the github repo
+  url = 'https://github.com/braingineer/pyromancy',
   download_url = 'https://github.com/braingineer/pyromancy/archive/0.1.tar.gz',
-  keywords = ['deep learning', 'pytorch'], # arbitrary keywords
+  keywords = ['deep learning', 'pytorch'],
   classifiers = [],
 )
